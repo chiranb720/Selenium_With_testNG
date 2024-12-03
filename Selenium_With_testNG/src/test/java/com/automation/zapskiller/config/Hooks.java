78311a044b1d44aa3f5ef4c3181653a96c7ce249
Chiranth <chiranthpb3@gmail.com>
2024-12-03 20:45:49 +0530

    Addded comments

--- Selenium_With_testNG/src/test/java/com/automation/zapskiller/config/Hooks.java
+++ Selenium_With_testNG/src/test/java/com/automation/zapskiller/config/Hooks.java
@@ -1,18 +1,64 @@
 package com.automation.zapskiller.config;
 
+import com.automation.zapskiller.utils.UIAutomationUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 import java.util.Properties;
 
 public class Hooks {
     public static final Logger logger = LoggerFactory.getLogger(Hooks.class);
     public static Properties configProps;
+    public WebDriver driver;
+    public ChromeOptions options = new ChromeOptions();
 
     @BeforeSuite
     public void BeforeSuite(){
+        configProps = UIAutomationUtils.readConfig();
+    }
 
+    @BeforeTest
+    public void beforeTest(){
+        launchBrowser();
+
+    }
+
+    @AfterTest
+    public void afterTest(){
+        closeBrowser();
+    }
+
+
+    /**
+     * <p>Launches a Browser Based On Provided Config Params</p>
+     * @Version 1.0
+     */
+    public void launchBrowser(){
+        if(configProps.getProperty("browser").equalsIgnoreCase("browser")){
+            if(configProps.getProperty("browser.chrome.options.headless").equalsIgnoreCase("true")){
+                options.addArguments("--headless");
+            }
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+        }
+
+    }
+
+
+    /**
+     * <p>This Method Is Used To Terminate Webdriver Session</p>
+     * @version 1.0
+     */
+    public void closeBrowser(){
+
+        driver.quit();
     }
 
 
